2774b62b3931db02c06fbdbcafc4678e31db38cf
Md Siddique Ahmad <md-ahmad@newgensoft.com>
2025-03-17 10:37:09 +0530

    update

--- 100xDev/basicJavascript.js
+++ 100xDev/basicJavascript.js
@@ -1,6 +1,17 @@
-var a=1;
-a=true;
-console.log(a);
 
-let users = ["siisdiq","ahmad"];
-console.log(users[0]);
+
+// let user=["siddique",21,{
+//     "roll":21,
+//     "enroll":{
+//         "en1":1,
+//         "en":2
+//     }
+// }]
+
+// console.log(user[2].enroll.en1);
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(2,3));
